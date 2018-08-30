b27870d04a89a5fd45c023f9870ccadc74b870da
StandardGiraffe <dannyfekete@gmail.com>
2018-08-30 19:07:22 +0000

    Built empty function and copied testing code from Compass.

---
+++ callbacks-map.js
@@ -0,0 +1,27 @@
+function map (array, callback) {
+
+}
+
+
+
+
+
+
+
+
+
+// ## Testing Zone ##
+
+var words = ["ground", "control", "to", "major", "tom"];
+
+map(words, function(word) {
+  return word.length;
+});
+
+map(words, function(word) {
+  return word.toUpperCase();
+});
+
+map(words, function(word) {
+  return word.split('').reverse().join('');
+});
